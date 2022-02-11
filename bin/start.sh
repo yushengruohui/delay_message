735ed24079fe3a9e67738bfff7ba0ed37877bfc0
yushengruohui <39661172+yushengruohui@users.noreply.github.com>
2022-02-11 21:07:53 +0800

    build: 优化打包配置

--- bin/start.sh
+++ bin/start.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
-pwd_bak=$(pwd)
+echo "start.sh running"
 # 获取脚本所在目录
-bin_dir=$(dirname $(readlink -f "$0"))
+bin_dir=$(dirname "$(readlink -f "$0")")
 echo "bin_dir : ${bin_dir}"
 # 项目基本目录
 base_dir=$(dirname "${bin_dir}")
 echo "base_dir : ${base_dir}"
 # 项目名，建议配置为 maven 的默认jar命名风格
-project_name=$(basename ${base_dir})
+tar_name=$(basename "${base_dir}")
+project_name=${tar_name%-*}
 echo "project_name : ${project_name}"
 # jar包所在目录
 lib_dir=${base_dir}/lib
 echo "lib_dir : ${lib_dir}"
 # 检查程序是否已启动，如果已启动，则结束它
-check_pid=$(ps -ef | grep java | grep ${project_name} | grep -v grep | awk '{print $2}')
+check_pid=$(ps -ef | grep java | grep "${project_name}" | grep -v grep | awk '{print $2}')
 if [ x"${check_pid}" != x"" ]; then
   echo "${project_name} is running.pid is ${check_pid}. try kill it"
-  kill -15 ${check_pid}
+  kill -15 "${check_pid}"
   sleep 10s
-  kill -9 ${check_pid}
+  kill -9 "${check_pid}"
 fi
+# 初始化基础环境
 source /etc/profile
+ulimit -n 640000
+ulimit -u 640000
+# 配置jvm的堆内存
+total_men=$(head /proc/meminfo -n 1 | awk '{print $2}')
+echo "total_men : ${total_men} kb"
+jvm_men=$(("${total_men}" / 1024 / 4))
+if [ ${jvm_men} -lt 1024 ]; then jvm_men=1024; fi
+echo "jvm_men : ${jvm_men} mb"
 jvm_config="\
--Xms2g \
--Xmx2g \
+-Xms${jvm_men}m \
+-Xmx${jvm_men}m \
 -Xss256k \
 -server \
 -Djava.security.edg=file:/dev/./urandom \
@@ -31,7 +41,7 @@ jvm_config="\
 -XX:+HeapDumpOnOutOfMemoryError \
 -XX:HeapDumpPath=${base_dir}/ \
 "
-cd ${lib_dir}
-nohup java ${jvm_config} -jar ${lib_dir}/${project_name}.jar >/dev/null 2>&1 &
+cd ${base_dir} || (echo "lib_dir not exist" && exit 1)
+#nohup java ${jvm_config} -jar ${lib_dir}/${tar_name}.jar >/dev/null 2>&1 &
+java ${jvm_config} -jar ${lib_dir}/${tar_name}.jar
 echo "${project_name} has started ,but may be exist exception.please check log"
-cd ${pwd_bak}
--- bin/stop.sh
+++ bin/stop.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-pwd_bak=$(pwd)
 # 获取脚本所在目录
-bin_dir=$(dirname $(readlink -f "$0"))
+echo "stop.sh running"
+bin_dir=$(dirname "$(readlink -f "$0")")
 echo "bin_dir : ${bin_dir}"
 # 项目基本目录
 base_dir=$(dirname "${bin_dir}")
 echo "base_dir : ${base_dir}"
 # 项目名，建议配置为 maven 的默认jar命名风格
-project_name=$(basename ${base_dir})
+tar_name=$(basename "${base_dir}")
+project_name=${tar_name%-*}
 echo "project_name : ${project_name}"
 # 检查程序是否已启动，如果已启动，则结束它
-check_pid=$(ps -ef | grep java | grep ${project_name} | grep -v grep | awk '{print $2}')
-if [ x"check_pid" != x"" ]; then
-  echo "${project_name} is running. try kill it"
-  kill -15 ${check_pid}
-  sleep 10
-  kill -9 ${check_pid}
+check_pid=$(ps -ef | grep java | grep "${project_name}" | grep -v grep | awk '{print $2}')
+if [ x"${check_pid}" != x"" ]; then
+  echo "${project_name} is running.pid is ${check_pid}. try kill it"
+  kill -15 "${check_pid}"
+  sleep 10s
+  kill -9 "${check_pid}"
+  echo "stop is ok"
 fi
